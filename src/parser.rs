325d928e1ae80feac69ac353332789d49cda8382
plax-00 <plaxman81@gmail.com>
2025-02-17 00:31:19 -0500

    some error handling

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,10 @@
 use std::iter::Peekable;
 
+use anyhow::Result;
+
 use crate::{
-    expression::{Binary, Expression, Grouping, Literal, Operator, Unary},
+    expression::{Binary, Expression, Grouping, Literal, Unary},
+    operator::{BinaryOperator, UnaryOperator},
     token::{Token, TokenType, Tokens},
 };
 
@@ -17,61 +20,64 @@ impl Parser {
         Self { tokens, current }
     }
 
-    fn parse_expr(&mut self) -> Expression {
-        self.parse_recursive(0)
+    pub fn parse(&mut self) -> Result<Expression> {
+        self.parse_expr(0)
     }
 
-    fn parse_recursive(&mut self, right_bp: u8) -> Expression {
-        let mut expr = self.parse_head();
+    fn parse_expr(&mut self, right_bp: u8) -> Result<Expression> {
+        let mut expr = self.parse_head()?;
         while let Some(op) = self
             .tokens
             .peek()
-            .and_then(|t| Operator::try_from(t).ok())
-            .filter(|o| infix_binding_power(&o) > right_bp)
+            .and_then(|t| BinaryOperator::try_from(t).ok())
+            .filter(|o| infix_binding_power(o) > right_bp)
         {
             self.tokens.next();
-            expr = self.parse_tail(expr, op);
+            expr = self.parse_tail(expr, op.into())?;
         }
-        expr
+        Ok(expr)
     }
 
     #[inline]
-    fn parse_head(&mut self) -> Expression {
+    fn parse_head(&mut self) -> Result<Expression> {
         if let Some(ref t) = self
             .tokens
             .next_if(|t| matches!(t.token_type, TokenType::Minus | TokenType::Bang))
         {
             let operator = t.try_into().unwrap();
-            let expr = Box::new(self.parse_recursive(prefix_binding_power(&operator)));
-            Unary { operator, expr }.into()
+            let operator_bp = prefix_binding_power(&operator);
+            let expr = self.parse_expr(operator_bp).map(Box::new)?;
+            let unary = Unary { operator, expr }.into();
+            Ok(unary)
         } else {
             self.parse_primary()
         }
     }
 
     #[inline]
-    fn parse_tail(&mut self, left: Expression, op: Operator) -> Expression {
+    fn parse_tail(&mut self, left: Expression, op: BinaryOperator) -> Result<Expression> {
         let operator_bp = infix_binding_power(&op);
-        let right = self.parse_recursive(operator_bp);
-        Binary {
+        let right = self.parse_expr(operator_bp)?;
+        let expr = Binary {
             operator: op,
             left: Box::new(left),
             right: Box::new(right),
         }
-        .into()
+        .into();
+        Ok(expr)
     }
 
     #[inline]
-    fn parse_primary(&mut self) -> Expression {
+    fn parse_primary(&mut self) -> Result<Expression> {
         let t = self.tokens.next().expect("Expected token(s) to parse.");
-        match t.token_type {
+        let expr = match t.token_type {
             TokenType::True => Literal::True.into(),
             TokenType::False => Literal::False.into(),
             TokenType::Nil => Literal::Nil.into(),
             TokenType::Number(n) => Literal::Number(n).into(),
             TokenType::String(s) => Literal::String(s).into(),
             TokenType::LeftParen => {
-                let expr = self.parse_expr();
+                let expr = self.parse()?;
                 let Some(Token {
                     token_type: TokenType::RightParen,
                     ..
@@ -85,12 +91,18 @@ impl Parser {
                 .into()
             }
             _ => panic!("{}", t),
-        }
+        };
+        Ok(expr)
+    }
+
+    fn synchronize(&mut self) {
+        todo!()
     }
 }
 
 enum BindingPower {
-    Equality = 1,
+    AndOr = 1,
+    Equality,
     Comparison,
     Additive,
     Multiplicative,
@@ -99,21 +111,20 @@ enum BindingPower {
 
 fn infix_binding_power(op: &BinaryOperator) -> u8 {
     let bp = match *op {
-        Operator::EqualEqual | Operator::NotEqual => BindingPower::Equality,
-        Operator::Greater | Operator::Less | Operator::GreaterEqual | Operator::LessEqual => {
-            BindingPower::Comparison
-        }
-        Operator::Minus | Operator::Plus => BindingPower::Additive,
-        Operator::Mult | Operator::Div => BindingPower::Multiplicative,
+        BinaryOperator::EqualEqual | BinaryOperator::NotEqual => BindingPower::Equality,
+        BinaryOperator::Greater
+        | BinaryOperator::Less
+        | BinaryOperator::GreaterEqual
+        | BinaryOperator::LessEqual => BindingPower::Comparison,
+        BinaryOperator::Minus | BinaryOperator::Plus => BindingPower::Additive,
+        BinaryOperator::Mult | BinaryOperator::Div => BindingPower::Multiplicative,
+        BinaryOperator::And | BinaryOperator::Or => BindingPower::AndOr,
         _ => todo!(),
     };
     bp as u8
 }
 
-fn prefix_binding_power(op: &Operator) -> u8 {
-    let (Operator::Minus | Operator::Not) = *op else {
-        panic!()
-    };
+fn prefix_binding_power(_op: &UnaryOperator) -> u8 {
     BindingPower::Unary as u8
 }
 
@@ -126,13 +137,13 @@ mod tests {
 
     fn parse_source(source: &'static str) -> Expression {
         let tokens = Scanner::new(source.to_string()).scan_source().unwrap();
-        Parser::new(tokens).parse_expr()
+        Parser::new(tokens).parse().unwrap()
     }
 
     #[test]
     fn test_parse_expr() {
-        let expr = parse_source("2 + 3");
-        assert_eq!("(+ 2 3)", PRINTER.print(&expr));
+        let expr = parse_source("2 + 3 + 4");
+        assert_eq!("(+ (+ 2 3) 4)", PRINTER.print(&expr));
 
         let expr = parse_source("-123 * (45.67)");
         assert_eq!("(* (- 123) (group 45.67))", PRINTER.print(&expr));
@@ -160,5 +171,14 @@ mod tests {
 
         let expr = parse_source("4 == 2 - -2");
         assert_eq!("(== 4 (- 2 (- 2)))", PRINTER.print(&expr));
+
+        let expr = parse_source("true and !false");
+        assert_eq!("(and true (! false))", PRINTER.print(&expr));
+
+        let expr = parse_source("2 + 2 == 4 and true and 3 <= 4");
+        assert_eq!(
+            "(and (and (== (+ 2 2) 4) true) (<= 3 4))",
+            PRINTER.print(&expr)
+        );
     }
 }
